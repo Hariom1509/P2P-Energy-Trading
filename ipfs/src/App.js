3d03663a114efd582ae332320beb1c3a6ee67176
Hariom1509 <hariomvyas1509@gmail.com>
2023-03-30 17:05:05 +0530

    ipfs changes

--- ipfs/src/App.js
+++ ipfs/src/App.js
@@ -3,10 +3,13 @@ import {useState} from 'react';
 import axios from 'axios';
 
 function App() {
-  const [fileImg, setFileImg] = useState(null);
-  console.log(process.env.REACT_APP_PINATA_API_KEY)
-  const sendFileToIPFS = async (e) => {
 
+  const [fileImg, setFileImg] = useState(null);
+  const [hash, setHash] = useState('');
+  console.log(process.env.REACT_APP_PINATA_API_KEY);
+
+  const sendFileToIPFS = async (event) => {
+    event.preventDefault();
     if (fileImg) {
       console.log(fileImg);
             const formData = new FormData();
@@ -23,18 +26,20 @@ function App() {
                 }
               }
               ).then(function (response) {
-                alert(response.data.IpfsHash)
+                alert(response.data.IpfsHash);
+                setHash(response.data.IpfsHash);
+                console.log(hash);
             }).catch(function (error) {
                 alert(error)
             });
-  //Take a look at your Pinata Pinned section, you will see a new file added to you list.   
     }
   }
   return (
     <div className="App">
+      <h1>{hash}</h1>
       <form onSubmit={sendFileToIPFS}>
-        <input type="file" onChange={(e) =>setFileImg(e.target.files[0])} required />
-        <button type='submit' >Mint NFT</button>            
+        <input type="file" onChange={(event) =>setFileImg(event.target.files[0])} required />
+        <button type="submit" >Send</button>            
       </form>
     </div>
   );
